8f296482fd8bb5da6c4e19e4131db4f83a22ac87
郭亭巖 <kuotinyen@gmail.com>
2019-06-01 13:30:52 +0800

    Init commit

---
+++ ujob-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ujob-ios
+//
+//  Created by wk on 2019/6/1.
+//  Copyright © 2019 wk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
